d3f6a9e0e0c432eab12c6bc66599165a99aea92e
Konrad Piekutowski <konrad.piekutowski@amberbit.com>
2024-08-07 15:18:02 +0200

    Version bump: v0.2.6

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Spike.MixProject do
     [
       app: :spike,
       description: @description,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
